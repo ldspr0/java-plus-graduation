44335fa7b32c150251b50458c695f85b72627b8b
Alexander.Ivanov <alexander.ivanov@vrpconsulting.com>
2025-11-17 10:48:18 +0400

    Add some main changes to structure

--- stats/stats-server/src/main/java/ru/yandex/practicum/stats/server/exception/ErrorResponse.java
+++ stats/stats-server/src/main/java/ru/yandex/practicum/stats/server/exception/ErrorResponse.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.stats.server.exception;
 
 public class ErrorResponse {
-    String error;
+    private String error;
 
     public ErrorResponse(String error) {
         this.error = error;
