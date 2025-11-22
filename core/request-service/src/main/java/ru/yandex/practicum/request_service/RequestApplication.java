68daee4db90384c69f9133739bf4807320f9e5f8
Alexander.Ivanov <alexander.ivanov@vrpconsulting.com>
2025-11-23 00:40:20 +0400

    fixing issue with request route

--- core/request-service/src/main/java/ru/yandex/practicum/request_service/RequestApplication.java
+++ core/request-service/src/main/java/ru/yandex/practicum/request_service/RequestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {
-        "ru.yandex.practicum.user_service",
+        "ru.yandex.practicum.request_service",
         "ru.yandex.practicum.core_api.exception"
 })
 @EnableDiscoveryClient
