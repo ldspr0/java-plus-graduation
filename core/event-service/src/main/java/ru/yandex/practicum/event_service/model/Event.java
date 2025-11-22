2bba3f2e10b2f569795991fb03ec40d29dd49c1a
Alexander.Ivanov <alexander.ivanov@vrpconsulting.com>
2025-11-22 20:26:14 +0400

    updated Event entity

--- core/event-service/src/main/java/ru/yandex/practicum/event_service/model/Event.java
+++ core/event-service/src/main/java/ru/yandex/practicum/event_service/model/Event.java
@@ -1,19 +1,7 @@
 package ru.yandex.practicum.event_service.model;
 
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -75,6 +63,4 @@ public class Event {
 
     @Convert(converter = EventStateConverter.class)
     private EventState state;
-
-    private List<Long> requestsIds = new ArrayList<>();
 }
